7040f6b22a9c0a3948bf109e4a37ff8c7c1cddfc
Joseph Vigneau <joev@alum.wpi.edu>
2018-12-01 10:42:18 -0500

    Solved AoC 2018 Day 1.
    Note: This needs refactoring, unit tests to validate samples, etc.

--- 2018/aoc2018/main.go
+++ 2018/aoc2018/main.go
@@ -3,5 +3,8 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("Advent of Code 2018: Go Edition\n");
+	fmt.Println("Advent of Code 2018: Go Edition")
+
+	fmt.Println("Day 1, part 1: ", day1part1(readFile("data/day1.txt")))
+	fmt.Println("Day 1, part 2: ", day1part2(readFile("data/day1.txt")))
 }
