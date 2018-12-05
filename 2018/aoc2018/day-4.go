8dbcf0a424a5aab2912affbeb0ffe194e1b5e3a5
Joe Vigneau <joev@alum.wpi.edu>
2018-12-04 20:47:01 -0500

    Day 4: Reduce sample duplication

--- 2018/aoc2018/day-4.go
+++ 2018/aoc2018/day-4.go
@@ -64,7 +64,7 @@ func day4() Puzzle {
 		return max, maxI
 	}
 
-	samples1 := map[string]string{
+	sample :=
 		`[1518-11-01 00:00] Guard #10 begins shift
 [1518-11-01 00:05] falls asleep
 [1518-11-01 00:25] wakes up
@@ -82,7 +82,8 @@ func day4() Puzzle {
 [1518-11-05 00:03] Guard #99 begins shift
 [1518-11-05 00:45] falls asleep
 [1518-11-05 00:55] wakes up
-`: "240"}
+`
+	samples1 := map[string]string{sample: "240"}
 	part1 := func(in io.Reader) string {
 		naps := parse(in)
 		mostGuard := 0
@@ -107,25 +108,7 @@ func day4() Puzzle {
 		return strconv.Itoa(mostGuard * maxMinute)
 	}
 
-	samples2 := map[string]string{
-		`[1518-11-01 00:00] Guard #10 begins shift
-[1518-11-01 00:05] falls asleep
-[1518-11-01 00:25] wakes up
-[1518-11-01 00:30] falls asleep
-[1518-11-01 00:55] wakes up
-[1518-11-01 23:58] Guard #99 begins shift
-[1518-11-02 00:40] falls asleep
-[1518-11-02 00:50] wakes up
-[1518-11-03 00:05] Guard #10 begins shift
-[1518-11-03 00:24] falls asleep
-[1518-11-03 00:29] wakes up
-[1518-11-04 00:02] Guard #99 begins shift
-[1518-11-04 00:36] falls asleep
-[1518-11-04 00:46] wakes up
-[1518-11-05 00:03] Guard #99 begins shift
-[1518-11-05 00:45] falls asleep
-[1518-11-05 00:55] wakes up
-`: "4455"}
+	samples2 := map[string]string{sample: "4455"}
 
 	part2 := func(in io.Reader) string {
 		napCount := make(map[int][]int)
